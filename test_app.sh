73b18e7db2fd8a0c657edf88d7b925cbb4496e3c
denisttyy <104391424+denisttyy@users.noreply.github.com>
2022-08-03 15:16:27 +0300

    Update test_app.sh

--- test_app.sh
+++ test_app.sh
@@ -4,13 +4,10 @@ ARCH=""
 
 if [ $# -eq 0 ]; then
     ARCH="x86_64"
-elif [ $1 = "arm" ]; then
     ARCH="arm64"
 fi
 
 echo "Building with arch: ${ARCH}"
-
-export LC_CTYPE=en_US.UTF-8
 
 set -o pipefail && arch -"${ARCH}" xcodebuild -workspace CodeEdit.xcworkspace \
            -scheme CodeEdit \
